30c40ddba317ccea94c6b91a39ccbdef8663c28b
Yoeri op't Roodt <yoeri.optroodt@ordina.be>
2023-04-05 15:18:35 +0200

    124. Creating & Using an "Autobind" decorator

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    const adjustedDescriptor: PropertyDescriptor = {
+        configurable: true,
+        get() {
+            return originalMethod.bind(this);
+        }
+    };
+
+    return adjustedDescriptor;
+}
+
 class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
@@ -22,13 +34,14 @@ class ProjectInput {
         this.attach();
     }
 
+    @autobind
     private submitHandler(event: Event) {
         event.preventDefault();
         console.log(this.titleInputElement.value);
     }
 
     private configure() {
-        this.element.addEventListener('submit', this.submitHandler.bind(this))
+        this.element.addEventListener('submit', this.submitHandler)
     }
 
     private attach() {
