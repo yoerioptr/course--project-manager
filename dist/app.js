cb5346528591009fdfe9b966cc4fe8f046db2b08
Yoeri op't Roodt <yoeri.optroodt@ordina.be>
2023-04-06 09:56:13 +0200

    127. Rendering Project Lists

--- dist/app.js
+++ dist/app.js
@@ -41,6 +41,26 @@ function autobind(_, _2, descriptor) {
     };
     return adjustedDescriptor;
 }
+class ProjectList {
+    constructor(type) {
+        this.type = type;
+        this.templateElement = document.getElementById('project-list');
+        this.hostElement = document.getElementById('app');
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.element.id = `${this.type}-projects`;
+        this.attach();
+        this.renderContent();
+    }
+    renderContent() {
+        const listId = `${this.type}-project-list`;
+        this.element.querySelector('ul').id = listId;
+        this.element.querySelector('h2').textContent = this.type.toUpperCase() + ' PROJECTS';
+    }
+    attach() {
+        this.hostElement.insertAdjacentElement('beforeend', this.element);
+    }
+}
 class ProjectInput {
     constructor() {
         this.templateElement = document.getElementById('project-input');
@@ -103,5 +123,7 @@ class ProjectInput {
 __decorate([
     autobind
 ], ProjectInput.prototype, "submitHandler", null);
-const projectInput = new ProjectInput();
+new ProjectInput();
+new ProjectList('active');
+new ProjectList('finished');
 //# sourceMappingURL=app.js.map
